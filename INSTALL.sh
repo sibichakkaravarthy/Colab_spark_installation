89c6bef545eb686f8a9e3ce99dee48a63fc6de38
Sibi Chakkaravarthy S <sb.sibi@gmail.com>
2019-11-06 17:41:50 +0530

    dataset

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,3 +5,7 @@ apt-get update -qq;
 !wget -q https://www-us.apache.org/dist/spark/spark-2.4.1/spark-2.4.1-bin-hadoop2.7.tgz
 !tar xf spark-2.4.1-bin-hadoop2.7.tgz
 !pip install -q findspark
+
+echo "import os"
+echo "os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64""
+echo "os.environ["SPARK_HOME"] = "/content/spark-2.3.2-bin-hadoop2.7""
