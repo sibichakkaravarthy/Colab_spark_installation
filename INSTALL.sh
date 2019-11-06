23ef5c977f4dd04cc5de1417a87aad5875abe2ae
Sibi Chakkaravarthy S <sb.sibi@gmail.com>
2019-11-06 17:52:07 +0530

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,7 @@
-!apt install openjdk-8-jdk-headless -qq > /dev/null
-!wget -q https://www-us.apache.org/dist/spark/spark-2.4.1/spark-2.4.1-bin-hadoop2.7.tgz
-!tar xf spark-2.4.1-bin-hadoop2.7.tgz
-!pip install -q findspark
+apt install openjdk-8-jdk-headless -qq > /dev/null
+wget -q https://www-us.apache.org/dist/spark/spark-2.4.1/spark-2.4.1-bin-hadoop2.7.tgz
+tar xf spark-2.4.1-bin-hadoop2.7.tgz
+pip install -q findspark
 
 echo "import os"
 echo "os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64""
