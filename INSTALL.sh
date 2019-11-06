ed786070e8fb3e85a3f0794df39fe456eb7b39f0
Sibi Chakkaravarthy S <sb.sibi@gmail.com>
2019-11-06 17:27:25 +0530

    Install.sh

---
+++ INSTALL.sh
@@ -0,0 +1,4 @@
+!apt-get install openjdk-8-jdk-headless -qq > /dev/null
+!wget -q https://www-us.apache.org/dist/spark/spark-2.4.1/spark-2.4.1-bin-hadoop2.7.tgz
+!tar xf spark-2.4.1-bin-hadoop2.7.tgz
+!pip install -q findspark
