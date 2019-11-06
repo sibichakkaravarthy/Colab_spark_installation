2e898555da085a9ce1399f71a33ee05fda5fb78f
Sibi Chakkaravarthy S <sb.sibi@gmail.com>
2019-11-06 17:32:11 +0530

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,6 @@
+dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb;
+apt-key add /var/cuda-repo-8-0-local-ga2/7fa2af80.pub;
+apt-get update -qq;
 !apt-get install openjdk-8-jdk-headless -qq > /dev/null
 !wget -q https://www-us.apache.org/dist/spark/spark-2.4.1/spark-2.4.1-bin-hadoop2.7.tgz
 !tar xf spark-2.4.1-bin-hadoop2.7.tgz
